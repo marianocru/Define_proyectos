4eb1e2b2606510bfd2f90be0e1657b1967941a3a
marianocru <marianocru@hotmail.com>
2013-03-23 11:29:58 -0300

    3er commit con test de Tareas

--- features/step_definitions/historia.rb
+++ features/step_definitions/historia.rb
@@ -2,14 +2,6 @@ Given /^Voy a la pagina de alta historia$/ do
   visit new_historia_path
 end
 
-Given /Ingreso in "([^"]*)" with "([^"]*)"$/ do |campo, valor|
-  fill_in(campo, :with => valor)
-end
-
-When /^Presiono "(.*)"$/ do |boton|
-  click_button boton
-end
-
 Then /^Espero ver la pagina de datos de la historia del proyecto$/ do
   page.should have_content('La nueva historia ha sido registrada.')
 end
