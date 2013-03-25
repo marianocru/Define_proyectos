704bc3c7e24801fff068ae66e8fae5a6cbf25680
marianocru <marianocru@hotmail.com>
2013-03-25 18:43:43 -0300

    un commit mas

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :establecer_idioma
+  before_filter :instanciar_controlador_accion
   layout 'layout'
+
+  def establecer_idioma
+    I18n.locale = :es
+  end
+
+  def instanciar_controlador_accion
+    @accion_actual = action_name
+    @controlador_actual = controller_name
+  end
+
+
 end
